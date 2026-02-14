dded517df0249eb968aa52cd16463d0c6d5cac94
Odair Michael Bendotti <obendotti@gmail.com>
2026-02-14 13:21:53 -0300

    feat: created Header component

---
+++ front-end/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { Image } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="flex gap-2 items-center py-5 px-4 ">
+      <div className="bg-surface-logo p-2 rounded-lg">
+        <Image className="text-surface" />
+      </div>
+      <h1 className="font-bold">Image Optimizer</h1>
+    </div>
+  );
+};
+
+export default Header;
